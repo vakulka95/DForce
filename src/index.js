b258c551015cab3babad6728aa5ef62fef264ce9
BMKiyv <k1brusenets@gmail.com>
2020-10-06 19:17:27 +0300

    without css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/index.css';
-import './assets/fonts/CA Saygon Text/stylesheet.css';
-import './assets/fonts/Grandis/stylesheet.css';
+import './assets/App.scss';
 import App from './App';
 
 ReactDOM.render(
