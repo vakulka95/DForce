13538dd5082d7d0a7fae61701dcc2f7600c3b6bf
BMKiyv <k1brusenets@gmail.com>
2020-10-06 04:15:37 +0300

    without fonts using

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './assets/App.scss';
 
 function App() {
+  const paragraph = 'Time to work hardly'
   return (
     <div className="App">
       <h1>Let`s go!!!</h1>
+      <p>{paragraph}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/index.css';
+import './assets/App.scss';
 import App from './App';
 
 ReactDOM.render(
