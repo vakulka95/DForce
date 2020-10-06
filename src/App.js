a59faed65ea6715c101f17847734dccdaed9073f
Roma Vakulenko <vakulka95@gmail.com>
2020-10-06 15:29:34 +0300

    added footer in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/App.scss';
+import Footer from './layout/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Let`s go!!!</h1>
       <p>We must do it</p>
+      <Footer />
     </div>
   );
 }
