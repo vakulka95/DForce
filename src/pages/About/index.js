d2bcf6880d6925f80f57f14faba773f856470f8c
BMKiyv <k1brusenets@gmail.com>
2020-10-07 19:20:48 +0300

    about v1.0

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './style.scss';
+import SpecItem from '../../components/SpecItem/SpecItem';
+import about from '../../json/aboutItems.json';
 
 const About = () => {
+    console.log(about)
     return (
-        <div>ABOUT</div>
+        <section className = 'about'>
+            <h2>Наші цінності</h2>
+            <p>Ми створюємо тільки ті цифрові продукти, які приносять користь людям та допомагають компаніями бути більш ефективними. Наша команда зацікавлена у рості вашого бізнесу та довготривалій співпраці!</p>
+            <div>{
+               about.map((item) => 
+                <SpecItem key = {item.id}
+                title = {item.title}
+                id = {item.id}
+                image = {item.image}
+                alt = {item.alt}
+                text = {item.text}
+                />
+               ) 
+                
+                }
+
+            </div>
+        </section>
     )
 }
 
