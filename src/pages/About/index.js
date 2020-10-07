d2d5e4327e6d35430e915a1e2c66f360a5ded296
BMKiyv <k1brusenets@gmail.com>
2020-10-07 23:40:55 +0300

    v1.1 not right

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,25 +4,22 @@ import SpecItem from '../../components/SpecItem/SpecItem';
 import about from '../../json/aboutItems.json';
 
 const About = () => {
-    console.log(about)
+    console.log(about.map((item)=>item.image))
     return (
         <section className = 'about'>
-            <h2>Наші цінності</h2>
-            <p>Ми створюємо тільки ті цифрові продукти, які приносять користь людям та допомагають компаніями бути більш ефективними. Наша команда зацікавлена у рості вашого бізнесу та довготривалій співпраці!</p>
-            <div>{
-               about.map((item) => 
-                <SpecItem key = {item.id}
+            <h2 className = 'about-title'>Наші цінності</h2>
+            <p className = 'about-content'>Ми створюємо тільки ті цифрові продукти, які приносять користь людям та допомагають компаніями бути більш ефективними. Наша команда зацікавлена у рості вашого бізнесу та довготривалій співпраці!</p>
+            <div className = 'about-items'>{
+               about.map((item) => <div className='about-items-one'>
+                <SpecItem
+                 key = {item.id}
                 title = {item.title}
                 id = {item.id}
                 image = {item.image}
                 alt = {item.alt}
                 text = {item.text}
-                />
-               ) 
-                
-                }
-
-            </div>
+                /></div>
+               )}</div>
         </section>
     )
 }
