ae1f0c3c6d0e1f306698352f9efde3a7031baa46
BMKiyv <k1brusenets@gmail.com>
2020-10-08 02:28:01 +0300

    about v1.2

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 import SpecItem from '../../components/SpecItem/SpecItem';
-import about from '../../json/aboutItems.json';
+import about from '../../json/aboutItems.js';
 
 const About = () => {
     console.log(about.map((item)=>item.image))
@@ -10,7 +10,7 @@ const About = () => {
             <h2 className = 'about-title'>Наші цінності</h2>
             <p className = 'about-content'>Ми створюємо тільки ті цифрові продукти, які приносять користь людям та допомагають компаніями бути більш ефективними. Наша команда зацікавлена у рості вашого бізнесу та довготривалій співпраці!</p>
             <div className = 'about-items'>{
-               about.map((item) => <div className='about-items-one'>
+               about.map((item) => <div className='about-one' key={item.id}>
                 <SpecItem
                  key = {item.id}
                 title = {item.title}
