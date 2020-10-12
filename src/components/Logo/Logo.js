aa27f14ca290196db4ec3038a0d8a987e7fa957f
BMKiyv <k1brusenets@gmail.com>
2020-10-12 13:21:13 +0300

    logo link

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 import './style.scss';
 
 function Logo({logoClass, parentClass}) {
     return (
         <div className={parentClass}>
+            <Link to='/'>
             <img src='./images/logo.svg'  className={logoClass} alt='Logo'/>
+            </Link>
         </div>
     )
 }
