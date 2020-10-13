a226c889d14a02afd42c33c6488dca7c50cb39df
BMKiyv <k1brusenets@gmail.com>
2020-10-13 18:45:25 +0300

    with svg loading

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,11 +3,8 @@ import './style.scss';
 
 const Loader = () => {
     return (
-        <div class="loader-container arc-rotate2">
-            <div class="inner"><img height="100px" width="100px" alt="loading" src="./images/Ellipse.png"/></div>
-            <div class="loader">
-                <div class="arc"></div>
-            </div>
+        <div className="anim">
+            <img height="100px" alt="loading" width="100px" src="./images/load.svg" />
         </div>
  
     )
