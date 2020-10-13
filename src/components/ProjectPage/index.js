a21e26c8dee33a45fa9c4c4b3304dc04f35b7588
Roma Vakulenko <vakulka95@gmail.com>
2020-10-13 15:40:11 +0300

    rewrote class in details

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -8,55 +8,57 @@ function ProjectPage({item}) {
 
             return (<div className='container' key={item.title}>
 
-                <h2 className='title-of-block'>{item.title1}</h2>
-                <figure className='case-image margin-for-images-cases image-background padding-cases'>
+                <h2 className='detail-sub-title'>{item.title1}</h2>
+                <figure className='detail-figure color'>
                     <img alt='image' src={item.group1} key={item.group1} />
                 </figure>
                 <div className='right'>
-                    <h2 className='title-of-block '>{item.title2}</h2>
-                    <p className='text'>
+                    <h2 className='detail-sub-title '>{item.title2}</h2>
+                    <p className='detail-text'>
                         {item.paragraph1}
                     </p>
                 </div>
-                <figure className='case-image margin-for-images-cases image-background padding-cases'>
+                <figure className='detail-figure color flex'>
                     {item.group2.map((img) => <img alt={item.title} src={img} key={img} />)}
                 </figure>
                 <div>
-                    <h2 className='title-of-block'>{item.title2}</h2>
-                    <p className='text'>
+                    <h2 className='detail-sub-title'>{item.title2}</h2>
+                    <p className='detail-text'>
                         {item.paragraph2}
                     </p>
                 </div>
-                <figure className='case-image margin-for-images-cases indifferent-background padding-cases'>
-                    {item.group3.map((img) => <img alt={item.title} src={img} key={img} />)}
-                </figure>
-                <figure className='case-image margin-for-images-cases indifferent-background padding-cases'>
-                    {item.group4.map((img) => <img alt={item.title} src={img} key={img} />)}
-                </figure>
+                <div className='detail-block-desktop-figures'>
+                    <figure className='detail-figure dark'>
+                        {item.group3.map((img) => <img alt={item.title} src={img} key={img} />)}
+                    </figure>
+                    <figure className='detail-figure dark'>
+                        {item.group4.map((img) => <img alt={item.title} src={img} key={img} />)}
+                    </figure>
+                </div>
                 <div>
-                    <h2 className='title-of-block'>{item.title2}</h2>
-                    <p className='text'>
+                    <h2 className='detail-sub-title'>{item.title2}</h2>
+                    <p className='detail-text'>
                         {item.paragraph2}
                     </p>
                 </div>
-                <figure className='case-image margin-for-images-cases image-background padding-cases'>
+                <figure className='detail-figure color flex'>
                     {item.group5.map((img) => <img alt={item.title} src={img} key={img} />)}
                 </figure>
                 <div className='right'>
-                    <h2 className='title-of-block '>{item.title2}</h2>
-                    <p className='text'>
+                    <h2 className='detail-sub-title '>{item.title2}</h2>
+                    <p className='detail-text'>
                         {item.paragraph1}
                     </p>
                 </div>
-                <div className='flex margin-for-images-cases'>
-                    <figure className='case-image indifferent-background padding-cases'>
+                <div className='detail-block-figures flex'>
+                    <figure className='detail-figure dark'>
                         {item.group6.map((img) => <img alt={item.title} src={img} key={img} />)}
                     </figure>
-                    <figure className='case-image indifferent-background padding-cases'>
+                    <figure className='detail-figure dark'>
                         {item.group7.map((img) => <img alt={item.title} src={img} key={img} />)}
                     </figure>
                 </div>
-                <figure className='case-image image-background padding-cases'>
+                <figure className='detail-figure color'>
                     {item.group8.map((img) => <img alt={item.title} src={img} key={img} />)}
                 </figure>
                 
@@ -69,7 +71,7 @@ function ProjectPage({item}) {
 
     };
     return (
-        <section className='product-page main-padding'>
+        <section className='detail'>
             { renderLoop()}
         </section>
     );
