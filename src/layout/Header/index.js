7aec5d6ac0483f6d9ee46bca1b983b0054811d7a
BMKiyv <k1brusenets@gmail.com>
2020-10-14 18:29:02 +0300

    added mobileNav version1.0

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -5,15 +5,10 @@ import {useLocation} from 'react-router-dom';
 import './style.scss';
 
 const Header = () =>{
-    // const [mobileNav, setMobileNav] = useState({
-    //     display:'none'
-    // });
-
-    // const showMobileNav = () => {
-    //     setMobileNav({
-    //         display: 'Block'
-    //     })
-    // }
+   const [mobileNav, setMobileNav] = useState(false)
+   const activateMobileNav = () => {
+       setMobileNav((prev)=>!prev)
+   }
 
     const scrollWidthOffset = (el) => {
         const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
@@ -34,40 +29,45 @@ const Header = () =>{
     console.log(opasityHeader())
     let opasity = opasityHeader();
     return(
-        <header className = {opasity? 'header-opasity': 'header'}>
-            <div className='container header-container'>
+        <header className = {`header${!opasity? '': '-opasity'}${mobileNav? '-active': ''}`}>
+            <div className={`container header-container${mobileNav? '-active':''}`}>
                 <Logo parentClass='logo' logoClass='header-logo'/>
-            <div className = 'header-navigation'>
+            <div className = {`header-navigation${mobileNav? '-active': ''}`}>
                 <ul className = 'header-navigation-list'>
-                    <li className = 'header-navigation-item'>
+                    <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
                         <NavLink className = 'header-navigation-link'
+                        onClick={mobileNav? activateMobileNav:null}
                         smooth activeClassName = { 'header-navigation-link-active' }
                         to="/" ><span>Головна</span>
                         </NavLink></li>
-                    <li className = 'header-navigation-item'>
-                        <NavLink className = 'header-navigation-link' 
+                    <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
+                        <NavLink className = 'header-navigation-link'
+                        onClick={mobileNav? activateMobileNav:null} 
                         smooth activeClassName = { 'header-navigation-link-active' }
                         to="/#specialization" ><span>Послуги</span>
                         </NavLink></li>
-                    <li className = 'header-navigation-item'>
+                    <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
                         <NavLink className = 'header-navigation-link'
+                        onClick={mobileNav? activateMobileNav:null}
                         smooth activeClassName = { 'header-navigation-link-active' }
                         to="/#portfolio" ><span>Портфоліо</span>
                         </NavLink></li>
-                    <li className = 'header-navigation-item'>
-                        <NavLink className = 'header-navigation-link' 
+                    <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
+                        <NavLink className = 'header-navigation-link'
+                        onClick={mobileNav? activateMobileNav:null} 
                         smooth activeClassName = { 'header-navigation-link-active' }
                         to="/#about" ><span>Про нас</span>
                         </NavLink></li>
-                    <li className = 'header-navigation-item'>
-                        <NavLink className = 'header-navigation-link' 
+                    <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
+                        <NavLink className = 'header-navigation-link'
+                        onClick={mobileNav? activateMobileNav:null} 
                         activeClassName = { 'header-navigation-link-active' }
                         scroll={el => scrollWidthOffset(el)}
                         to="#forcustomer" ><span>Контакти</span>
                         </NavLink></li>
                 </ul>
             </div>
-            <div className = 'header-media'><div className = 'header-media-button'><img src="./images/mobileNav.svg" alt="button" /></div></div>
+            <div className = 'header-media'><div className = 'header-media-button' onClick={activateMobileNav}><img src="./images/mobileNav.svg" alt="button" /></div></div>
             </div>
         </header>
     )
