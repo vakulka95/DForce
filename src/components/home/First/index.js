9f3547f41ac7b41a03e682d100bb6a6eda17ca40
Roma Vakulenko <vakulka95@gmail.com>
2020-10-15 22:40:53 +0300

    added container in mob media

--- src/components/home/First/index.js
+++ src/components/home/First/index.js
@@ -3,14 +3,14 @@ import './style.scss';
 
 const First = () => {
     return (
-        <div className = 'home' id='home'>
+        <section className = 'home' id='home'>
             <div className='container'>
             <div className = 'home-wrap'>
               <h1 className = 'home-header'><span className = 'home-header-first'>Ми</span> створюємо майбутнє!</h1>
               <p className = 'home-paragraph'>Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.</p>
             </div>
             </div>
-        </div>
+        </section>
     )
 }
 
