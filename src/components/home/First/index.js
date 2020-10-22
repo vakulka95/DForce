81e9c916f24943c7f031fdcbe9986a4e993a784f
Roma Vakulenko <vakulka95@gmail.com>
2020-10-22 17:14:19 +0300

    added animation for 2 spans

--- src/components/home/First/index.js
+++ src/components/home/First/index.js
@@ -6,8 +6,12 @@ const First = () => {
         <section className = 'home' id='home'>
             <div className='container'>
             <div className = 'home-wrap'>
-              <h1 className = 'home-header'><span className = 'home-header-first'>Ми</span> створюємо майбутнє!</h1>
-              <p className = 'home-paragraph'>Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.</p>
+                <h1 className = 'home-header'><span className = 'home-header-first'>Ми </span> 
+                    <span className='animation-one'>створюємо майбутнє!</span>
+                     <span className='animation-two'>інженери</span>
+                   {/* <span className='animation-three'>якісний результат</span> */}
+                </h1>
+                <p className = 'home-paragraph'>Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя  та досвіду людей.</p>
             </div>
             </div>
         </section>
