6c82be4be401be58da9b4f5f3d8d2f529faab76b
Michael <k1brusenets@gmail.com>
2020-10-28 21:07:28 +0200

    added vote photo

--- src/json/portfolioItems.js
+++ src/json/portfolioItems.js
@@ -1,7 +1,7 @@
 let portfolioItems = [
     {
         id: 1,
-        image: './images/cases/Group-975.png',
+        image: './images/cases/itSeems-dt.png',
         alt: 'ItSeems',
         backgroundCLass: 'case-image itseems-background',
         description: 'Анонімний онлайн-щоденник для людей, які постраждали від насильства.',
@@ -9,7 +9,7 @@ let portfolioItems = [
     },
     {
         id: 2,
-        image: './images/cases/Group-977.png',
+        image: './images/cases/tellMe-dt.png',
         alt: 'Tellme',
         backgroundCLass: 'case-image tellme',
         description: 'Інтернет-платформа для психологічних консультацій, яка допомагає всім хто потребує емоційної підтримки.',
@@ -18,7 +18,7 @@ let portfolioItems = [
     },
     {
         id: 3,
-        image: './images/logo.svg',
+        image: './images/cases/vote-dt.png',
         alt: 'vote',
         backgroundCLass: 'case-image vote',
         description: 'Сервіс голосування в раду для МКІП повинен забезпечити ефективну та зручну взаємодію в процесі голосування',
