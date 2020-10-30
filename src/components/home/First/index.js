21b35d7654a246c8e3dfc0a1bb98cc87a29accb9
Roman <vakulka95@gmail.com>
2020-10-30 11:40:36 +0200

    added cube

--- src/components/home/First/index.js
+++ src/components/home/First/index.js
@@ -22,14 +22,26 @@ const First = () => {
         <section className = 'home' id='home' onMouseEnter={handleMouseMove}>
             <div className='container'>
                 <div className = 'home-wrap'>
-                    <h1 className = 'home-header'><span className = 'home-header-first'>Ми </span> 
-                        <span className='animation-one'>створюємо майбутнє!</span>
-                        <span className='animation-two'>інженери</span>
-                        <span className='animation-three'>= відмінний результат</span>
-                    </h1>
+                    <div className='home-intro'>
+                        <h1 className = 'home-header'><span className = 'home-header-first'>Ми </span> 
+                            <span className='animation-one'>створюємо майбутнє!</span>
+                            <span className='animation-two'>інженери</span>
+                            <span className='animation-three'>= відмінний результат</span>
+                        </h1>
 
-                    <p className = 'home-paragraph'>Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя та досвіду людей. Ми допомагаємо нашим клієнтам ефективно реалізувати найамбітніші ідеї!</p>
-                    {/* <button onClick={setHistory}>BUTTON</button> */}
+                        <p className = 'home-paragraph'>Dforce - це провідна ІТ-компанія, яка створює цифрові продукти для покращення життя та досвіду людей. Ми допомагаємо нашим клієнтам ефективно реалізувати найамбітніші ідеї!</p>
+                    </div>                                 
+                    <div className='home-animation'>
+                        <img src='images/animation/animation.svg' className='home-lego' alt='home-lego'/>
+                        {/* <div className='cube'>
+                            <div className='side front'>front</div>
+                            <div className='side back'>back</div>
+                            <div className='side right'>right</div>
+                            <div className='side left'>left</div>
+                            <div className='side top'>top</div>
+                            <div className='side bottom'>bottom</div>
+                        </div> */}
+                    </div>
                 </div>
             </div>
         </section>
