481e717c63be74fc84f3c43bc2eb2d8bb882e43f
Roman <vakulka95@gmail.com>
2020-11-06 15:11:59 +0200

    changed in-progress

--- src/components/PortfolioItem/PortfolioItem.js
+++ src/components/PortfolioItem/PortfolioItem.js
@@ -20,7 +20,9 @@ function PortfolioItems({ id,backgroundCLass, description, to , backgroundCLassD
             </Link>
             {disable ? 
             <div className='case-description'>
-                <div className='in-progress'>In progress</div>
+                {/* <div className='in-progress'> */}
+                    <img src='./images/cases/inProgressCase.svg' alt='inProgress' className='in-progress'/>
+                {/* </div> */}
                 <p className='case-title sub-title'>{descriptionDis}</p>
                 <div className='icons'>
                     {technologies.map((icon)=><img src={Object.values(icon).join('')} alt={Object.keys(icon).join('')} id={Object.keys(icon).join('')} title={Object.keys(icon).join('')} key={Object.keys(icon).join('')} />)}
