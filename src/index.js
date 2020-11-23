2fe627bd9d138cd3cf8ecbf2321dd9ed46a89ece
Roman <vakulka95@gmail.com>
2020-11-23 16:26:42 +0200

    returned files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 // import './assets/index.scss';
 // import './assets/fonts/CA Saygon Text/stylesheet.scss';
 import './assets/_fonts.scss';
 import './assets/iconsfont.scss';
 import App from './App';
+import './i18n/i18n';
 
 ReactDOM.render(
     <App />,
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -5,6 +5,7 @@ import {useLocation} from 'react-router-dom';
 import './style.scss';
 import Button from '../../components/Button';
 import {useOnScroll} from '../../utils/useOnScroll';
+import { useTranslation } from 'react-i18next';
 
 const Header = () =>{
     const [mobileNav, setMobileNav] = useState(false);
@@ -37,6 +38,12 @@ const Header = () =>{
     };
     let opasity = opasityHeader();
 
+    const { t } = useTranslation();
+
+    let changeLanguage = code => {
+        localStorage.setItem('language', code);
+        window.location.reload();
+    }
     return(
         <header style={usingScrolling?{background:"#0D0E12",zIndex:"500"}:null} className = {`header${!opasity? '': '-opasity'}${mobileNav? '-active': ''}`}>
             <div className={`container header-container${mobileNav? '-active':''}`}>
@@ -53,7 +60,7 @@ const Header = () =>{
                         <NavLink className = 'header-navigation-link'
                         onClick={mobileNav? activateMobileNav:null} 
                         smooth activeClassName = { 'header-navigation-link-active' }
-                        to="/#specialization" ><span>Послуги</span>
+                        to="/#specialization" ><span>Спеціалізація</span>
                         </NavLink></li>
                     <li className = {`header-navigation-item${mobileNav? '-active': ''}`}>
                         <NavLink className = 'header-navigation-link'
